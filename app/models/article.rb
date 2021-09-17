7676e44d00339b7c244d4ff073e6ead2c508c960
shuzo <zo.shu.0414.73@gmail.com>
2021-09-17 23:33:14 +0900

    投稿機能作成（複数画像）

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,6 @@
 #
 #  id         :bigint           not null, primary key
 #  content    :text
-#  image      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :bigint           not null
@@ -14,7 +13,7 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
-
+  has_many_attached :images
   belongs_to :user
 
 end
