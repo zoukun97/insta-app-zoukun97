cae45a6143b6a6c0bccf08a456504e1ced1f6f1f
shuzo <zo.shu.0414.73@gmail.com>
2021-10-17 23:40:11 +0900

    Revert "mailerの設定、letter-openerの追加"
    This reverts commit 340202cd283da8b14376ee3252a8ecff96bb9739.

--- app/mailers/comment_mailer.rb
+++
@@ -1,7 +0,0 @@
-class CommentMailer < ApplicationMailer
-  def new_comment(user, commenter)
-    @user = user
-    @commenter = commenter
-    mail to: user.email, subject: '【お知らせ】コメントされました'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'profiles#show'
