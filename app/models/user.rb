697564d77df504abf129a46e05d717de4f3970c0
shuzo <zo.shu.0414.73@gmail.com>
2021-10-20 00:54:23 +0900

    followを解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,4 +52,9 @@ class User < ApplicationRecord
     following_relationships.create!(following_id: user.id)
   end
 
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
+  end
+
 end
