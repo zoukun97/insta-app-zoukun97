22cf8774ad6eef3ab14776304f6cfa1308dbb665
shuzo <zo.shu.0414.73@gmail.com>
2021-10-20 00:32:32 +0900

    followerの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,12 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: "following_id", class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
 
   def avatar_image
     if profile&.avatar&.attached?
