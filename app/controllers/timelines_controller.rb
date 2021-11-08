4cbbd4be891bd189447f601123bc88bdcaa91df5
shuzo <zo.shu.0414.73@gmail.com>
2021-11-08 13:56:49 +0900

    24時間以内に作成された投稿の中で「いいね」が多い投稿を5つ

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,6 +3,14 @@ class TimelinesController < ApplicationController
 
   def show
     user_ids = current_user.followings.pluck(:id)
-    @articles = Article.where(user_id: [*user_ids, current_user.id]).order(created_at: :desc)
+    from = Time.now - 24.hours
+    to = Time.now
+    @articles = Article.where(user_id: [*user_ids, current_user.id], created_at: from..to).left_joins(:likes).group(:id).order('COUNT(likes.id) DESC',created_at: :desc).limit(5)
+    # created_at: 24.hours.ago..Time.now
+    # @articles = Article.joins(:likes).where(user_id: [*user_ids, current_user.id], created_at: from..to, likes: {article_id: 31}).order(created_at: :desc)
+    # @articles = Article.joins(:likes).where(created_at: 12.hours.ago..).order(created_at: :desc)
+    # @articles = Article.joins(:likes).where(created_at:from..to).where(Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+    # @articles = Article.where(created_at: from..to)
+    # @articles = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id)) && Article.where(created_at: from..to)
   end
 end
